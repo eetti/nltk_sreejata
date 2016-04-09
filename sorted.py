7c3f20b5f5b224aebb44d33ac36a7a84f8c17ac3
Emmanuel Etti <etti.emman@gmail.com>
2016-04-09 02:04:45 -0300

    Assignment update, used scikit learn for the tf idf

--- sorted.py
+++ sorted.py
@@ -2,12 +2,15 @@ import nltk
 import random
 from re import split
 from nltk.corpus import stopwords
+from nltk.stem.porter import *
 from nltk import FreqDist
 from nltk.tokenize import RegexpTokenizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from collections import Counter
 
 documents = []
-
-# 
+dict_ ={}
+ 
 read_file = "/Users/etti/Desktop/Sreejata/cnn_text.txt"
 
 with open(read_file,"r") as r:
@@ -16,35 +19,39 @@ with open(read_file,"r") as r:
 tokenizer = RegexpTokenizer(r'\w+')
 lowerCase = filecontent.lower()
 allwords = lowerCase
-# documents.append(allwords)
 filtered = tokenizer.tokenize(lowerCase)
 
 
-
-# '''
 for w in filtered:
 	# remove stop words and digits
-	if w not in stopwords.words() and not w.isdigit():
+	if w not in stopwords.words('english') and not w.isdigit():
 		documents.append(w)
-		# stopWords += w
-# '''
-# print stopwords
-print documents
 
-# local_numbers = [x for x in range(0,100)]
-# random.shuffle(local_numbers)
+		
+def stem_tokens(tokens, stemmer):
+    stemmed = []
+    for item in tokens:
+        stemmed.append(stemmer.stem(item))
+    return stemmed
 
-# print local_numbers
+stemmer = PorterStemmer()
+stemmed = stem_tokens(documents, stemmer)
 
-# print "amount" len(local_numbers) 
-# print documents
+tfidf = TfidfVectorizer(stop_words='english',max_features=10)
+tfs = tfidf.fit_transform(stemmed)
 
+names = tfidf.get_feature_names()
 
+msg = "Number of words in the bag of words  : %d" % len(stemmed)
 
+print msg
+print "_"*len(msg)
+print "Meaning full terms"
+print "_"*40
 
+for col in tfs.nonzero()[1]:
+	if names[col] not in dict_:
+		dict_[tfs[col]] = tfs[0,col]
+		print names[col],' - ',tfs[0,col]
 
-# [[x for x in range(y,y-3)] for y in range(1,11)]
-
-# stopwords = (for w in filecontent if w not in stopwords.words() and not w.isdigit())
-
-# print filtered
+# print tfs
